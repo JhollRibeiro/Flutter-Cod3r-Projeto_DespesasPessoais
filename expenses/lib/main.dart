f812541b7bcb97367cb64e84d435ac7129087425
JhollRibeiro <jrsantos.joao@gmail.com>
2023-03-24 10:26:58 -0300

    Seção 4 - App Despesas Pessoais - Aula 86 - Combinando Widgets | Colocar o Card na tela inteira (Alinhamentos do Column - center (padrão))

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -21,7 +21,7 @@ class MyHomePage extends StatelessWidget {
           title: const Text('Despesas Pessoais'),
         ),
         body: const Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             SizedBox(
               width: double.infinity,
