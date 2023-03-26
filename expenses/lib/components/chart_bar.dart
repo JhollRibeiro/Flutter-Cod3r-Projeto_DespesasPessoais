8f8a4cf38dbbdd9c29769c430769ffd0bf437b8c
JhollRibeiro <jrsantos.joao@gmail.com>
2023-03-26 01:27:58 -0300

    Seção 4 - App Despesas Pessoais - Aula 126 - Finalizando Chart Widget

--- expenses/lib/components/chart_bar.dart
+++ expenses/lib/components/chart_bar.dart
@@ -16,51 +16,47 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 5,
-            bottom: 7,
-          ),
+        FittedBox(
           child: Text(
             'R\$${value.toStringAsFixed(2)}',
-          ),
-        ),
-        SizedBox(
-          height: 80,
-          width: 10,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1,
-                  ),
-                  color: const Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: percentege,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-              )
-            ],
           ),
         ),
         Padding(
           padding: const EdgeInsets.only(
             top: 7,
-            bottom: 5,
+            bottom: 7,
           ),
-          child: Text(
-            label,
+          child: SizedBox(
+            height: 80,
+            width: 10,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                    color: const Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                FractionallySizedBox(
+                  heightFactor: percentege,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
+        ),
+        Text(
+          label,
         ),
       ],
     );
