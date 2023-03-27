019ddafd26b78b7e43db0255ec15db05e723b230
JhollRibeiro <jrsantos.joao@gmail.com>
2023-03-27 16:39:17 -0300

    Seção 4 - App Despesas Pessoais - Aula 129 - Melhorando Chart Widget

--- expenses/lib/components/chart_bar.dart
+++ expenses/lib/components/chart_bar.dart
@@ -16,9 +16,12 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(
-          child: Text(
-            value.toStringAsFixed(2),
+        SizedBox(
+          height: 20,
+          child: FittedBox(
+            child: Text(
+              value.toStringAsFixed(2),
+            ),
           ),
         ),
         Padding(
