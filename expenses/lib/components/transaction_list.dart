50abdb9499a7dc33007682f5b6470fc4dca59e23
JhollRibeiro <jrsantos.joao@gmail.com>
2023-04-03 10:13:11 -0300

    Seção 5 - App Despesas Pessoais - Aula 150. Aproveitando Larguras Maiores

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -75,11 +75,22 @@ class TransactionList extends StatelessWidget {
                     style: Theme.of(context).textTheme.titleLarge,
                   ),
                   subtitle: Text(DateFormat('d MMM y').format(tr.date)),
-                  trailing: IconButton(
-                    onPressed: () => onRemove(tr.id),
-                    icon: const Icon(Icons.delete),
-                    color: Colors.red,
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 480
+                      ? TextButton.icon(
+                          onPressed: () => onRemove(tr.id),
+                          icon: const Icon(Icons.delete),
+                          label: const Text(
+                            'Excluir',
+                            style: TextStyle(
+                              color: Colors.red
+                            ),
+                          ),
+                        )
+                      : IconButton(
+                          onPressed: () => onRemove(tr.id),
+                          icon: const Icon(Icons.delete),
+                          color: Colors.red,
+                        ),
                 ),
               );
             },
